6c6171ca09020ef1cd46dda170b5c70c0190c15c
skullY <skullydazed@gmail.com>
2020-04-28 22:57:04 -0700

    New release: 1.0.5 → 1.0.6

--- milc/__init__.py
+++ milc/__init__.py
@@ -12,7 +12,7 @@ For more details see the MILC documentation:
 
     <https://github.com/clueboard/milc/tree/master/docs>
 """
-__VERSION__ = '1.0.5'
+__VERSION__ = '1.0.6'
 
 import logging
 import os
