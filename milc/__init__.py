27a1225472cb856e217dba07474fbf89a64f1bb6
Zach White <skullydazed@gmail.com>
2021-03-28 12:29:14 +0000

    New release: 1.2.0 → 1.2.1

--- milc/__init__.py
+++ milc/__init__.py
@@ -12,7 +12,7 @@ For more details see the MILC documentation:
 
     <https://github.com/clueboard/milc/tree/master/docs>
 """
-__VERSION__ = '1.2.0'
+__VERSION__ = '1.2.1'
 
 import logging
 import os
