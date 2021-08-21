662ef8c6549c03b7c666e935709926bbfb5a4655
Zach White <skullydazed@gmail.com>
2021-08-21 08:30:44 -0700

    Add the ability to color sparklines to indicate positive and negative numbers

--- milc/_sparkline.py
+++ milc/_sparkline.py
@@ -13,8 +13,32 @@ def is_int(i):
     return isinstance(i, int)
 
 
-def sparkline(ints, int_min=None, int_max=None):
+def sparkline(ints, int_min=None, int_max=None, negative_color='{fg_red}', negative_reset='{fg_reset}', positive_color='', positive_reset='{fg_reset}'):
     """Display a sparkline from a sequence of ints.
+
+    If you wish to exclude extreme values, or you want to limit the set of characters used, you can adjust `int_min` and `int_max` to your own values. Values between your actual min/max will exclude datapoints, while values outside your actual min/max will compress your data into fewer sparks.
+
+    By default this function will display negative numbers in red and positive numbers in the system default color. You can use `negative_color`, `negative_reset`, `positive_color`, and `positive_reset` to change this behavior. If you wish to color your sparkline according to other rules it is recommended you modify it after generating it.
+
+    ### Arguments
+
+        int_min
+            The lowest value in your sparkline. If not provided it will be determined automatically.
+
+        int_max
+            The highest value in your sparkline. If not provided it will be determined automatically.
+
+        negative_color
+            A MILC or ANSI color code to apply to integers less than 0.
+
+        negative_reset
+            A MILC or ANSI color code to reset the color code applied in `negative_color`.
+
+        positive_color
+            A MILC or ANSI color code to apply to integers greater than 0.
+
+        positive_reset
+            A MILC or ANSI color code to reset the color code applied in `positive_color`.
     """
     int_min = int_min or min(filter(is_int, ints))
     int_max = int_max or max(filter(is_int, ints))
@@ -30,15 +54,15 @@ def sparkline(ints, int_min=None, int_max=None):
             cli.log.debug('Skipping out of bounds value %s', i)
             continue
 
-        spark_char = (i-int_min) / int_range * 8
+        spark_int = (i-int_min) / int_range * 8
 
-        if spark_char > 7:
-            spark_char = 7
+        if spark_int > 7:
+            spark_int = 7
 
-        sparks.append(spark_chars[min([7, int((i-int_min) / int_range * 8)])])
+        spark_char = spark_chars[int(spark_int)]
+        color = negative_color if i < 0 else positive_color
+        reset = negative_reset if i < 0 else positive_reset
+
+        sparks.append(''.join((color, spark_char, reset)))
 
     return ''.join(sparks)
-
-
-if __name__ == '__main__':
-    print(sparkline(list(range(101))))
--- tests/test_script_sparkline.py
+++ tests/test_script_sparkline.py
@@ -2,6 +2,6 @@ from .common import check_assert, check_command, check_returncode
 
 
 def test_sparkline():
-    result = check_command('./sparkline')
+    result = check_command('./sparkline', '--no-color')
     check_returncode(result)
-    check_assert(result, 'ℹ sparkline1: ▁▂▁▃ ▄▁█▂▄▃\nℹ sparkline2: ▁▁██\nℹ sparkline3: ▁▁▅▅██')
+    check_assert(result, result.stdout == 'ℹ sparkline1: ▄▄▃▅ ▆▃█▂▁▁\nℹ sparkline2: ▁▁██\nℹ sparkline3: ▁▁▅▅██\n')
