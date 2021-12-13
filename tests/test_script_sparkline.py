7a13c3fc3ae26209afce21d16d5852d9577b96e2
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-13 16:50:39 +0800

    Delete tests directory

--- tests/test_script_sparkline.py
+++
@@ -1,7 +0,0 @@
-from .common import check_assert, check_command, check_returncode
-
-
-def test_sparkline():
-    result = check_command('./sparkline', '--no-color')
-    check_returncode(result)
-    check_assert(result, result.stdout == 'ℹ sparkline1: ▄▄▃▅ ▆▃█▂▁▁\nℹ sparkline2: ▁▁██\nℹ sparkline3: ▁▁▅▅██\n')
