580161ddcad18dd61422979c1747c100d0fb986b
Jeff Epler <jepler@gmail.com>
2022-07-11 18:09:09 -0500

    Add more (failing) config source tests

--- tests/test_script_config_source.py
+++ tests/test_script_config_source.py
@@ -1,13 +1,57 @@
+import os
 from .common import check_assert, check_command, check_returncode
 
+WITH_CONFIG_FILE = ('--config-file', 'config_source.config')
+NO_CONFIG_FILE = ('--config-file',  os.path.devnull)
 
-def test_config_source():
-    result = check_command('./config_source')
+def test_config_source_config():
+    result = check_command('./config_source', *WITH_CONFIG_FILE)
+    check_returncode(result)
+    check_assert(result, result.stdout == 'Hello, Config, from config_file!\n')
+
+
+def test_config_source_no_config():
+    result = check_command('./config_source', *NO_CONFIG_FILE)
     check_returncode(result)
     check_assert(result, result.stdout == 'Hello, World, from None!\n')
 
 
-def test_config_source_name():
-    result = check_command('./config_source', '--name', 'World')
+def test_config_source_name_no_config():
+    result = check_command('./config_source', '--name', 'Test', *NO_CONFIG_FILE)
     check_returncode(result)
-    check_assert(result, result.stdout == 'Hello, World, from argument!\n')
+    check_assert(result, result.stdout == 'Hello, Test, from argument!\n')
+
+def test_config_source_name_long_1arg_no_config():
+    result = check_command('./config_source', '--name=Test', *NO_CONFIG_FILE)
+    check_returncode(result)
+    check_assert(result, result.stdout == 'Hello, Test, from argument!\n')
+
+def test_config_source_name_short_1arg_no_config():
+    result = check_command('./config_source', '-nTest', *NO_CONFIG_FILE)
+    check_returncode(result)
+    check_assert(result, result.stdout == 'Hello, Test, from argument!\n')
+
+def test_config_source_name_short_2arg_no_config():
+    result = check_command('./config_source', '-n', 'Test', *NO_CONFIG_FILE)
+    check_returncode(result)
+    check_assert(result, result.stdout == 'Hello, Test, from argument!\n')
+
+def test_config_source_name():
+    result = check_command('./config_source', '--name', 'Test', *WITH_CONFIG_FILE)
+    check_returncode(result)
+    check_assert(result, result.stdout == 'Hello, Test, from argument!\n')
+
+def test_config_source_name_long_1arg():
+    result = check_command('./config_source', '--name=Test', *WITH_CONFIG_FILE)
+    check_returncode(result)
+    check_assert(result, result.stdout == 'Hello, Test, from argument!\n')
+
+def test_config_source_name_short_1arg():
+    result = check_command('./config_source', '-nTest', *WITH_CONFIG_FILE)
+    check_returncode(result)
+    check_assert(result, result.stdout == 'Hello, Test, from argument!\n')
+
+def test_config_source_name_short_2arg():
+    result = check_command('./config_source', '-n', 'Test', *WITH_CONFIG_FILE)
+    check_returncode(result)
+    check_assert(result, result.stdout == 'Hello, Test, from argument!\n')
